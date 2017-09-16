878cc7969825d4015191df2cfe938dceebee3ce6
Nithin Buduma <nbuduma@mit.edu>
2017-09-16 15:41:04 -0400

    server code

--- server_extension.py
+++ server_extension.py
@@ -2,6 +2,14 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
+@app.route("/")
+def hi():
+    return "yo this works"
+
 @app.route("/add_event", methods=['GET', 'POST'])
 def add_event():
     request_data = request.get_data()
+    return request_data
+
+if __name__ == "__main__":
+	app.run()
