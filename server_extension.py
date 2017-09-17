c07b5fd3568f9a36bf62fcb7e76156f4c49d4675
Nithin Buduma <nbuduma@mit.edu>
2017-09-16 23:20:49 -0400

    changed spacing, removed comments

--- server_extension.py
+++ server_extension.py
@@ -13,109 +13,40 @@ app = flask.Flask(__name__)
 app.secret_key = "secret_key"
 
 
+@app.route('/')
+def index():
+    return "yo this works"
+
 @app.route('/add_event')
 def index():
-  if 'credentials' not in flask.session:
-    return flask.redirect(flask.url_for('oauth2callback'))
-  credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
-  if credentials.access_token_expired:
-    return flask.redirect(flask.url_for('oauth2callback'))
-  else:
-    http_auth = credentials.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http_auth)
-
-    '''now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    eventsResult = service.events().list(
-        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-        orderBy='startTime').execute()
-    events = eventsResult.get('items', [])
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])'''
-    return "done"
+    if 'credentials' not in flask.session:
+        return flask.redirect(flask.url_for('oauth2callback'))
+        credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+        if credentials.access_token_expired:
+            return flask.redirect(flask.url_for('oauth2callback'))
+        else:
+            http_auth = credentials.authorize(httplib2.Http())
+            service = discovery.build('calendar', 'v3', http_auth)
 
 
 @app.route('/oauth2callback')
 def oauth2callback():
-  flow = client.flow_from_clientsecrets(
-      'client_secret.json',
-      scope='https://www.googleapis.com/auth/calendar.readonly',
-      redirect_uri='https://flask-extension-server.herokuapp.com/oauth2callback')
-  if 'code' not in flask.request.args:
-    auth_uri = flow.step1_get_authorize_url()
-    return flask.redirect(auth_uri)
-  else:
-    auth_code = flask.request.args.get('code')
-    credentials = flow.step2_exchange(auth_code)
-    flask.session['credentials'] = credentials.to_json()
-    return flask.redirect(flask.url_for('index'))
+    flow = client.flow_from_clientsecrets(
+    'client_secret.json',
+    scope='https://www.googleapis.com/auth/calendar.readonly',
+    redirect_uri='https://flask-extension-server.herokuapp.com/oauth2callback')
+    if 'code' not in flask.request.args:
+        auth_uri = flow.step1_get_authorize_url()
+        return flask.redirect(auth_uri)
+    else:
+        auth_code = flask.request.args.get('code')
+        credentials = flow.step2_exchange(auth_code)
+        flask.session['credentials'] = credentials.to_json()
+        return flask.redirect(flask.url_for('index'))
 
 
 if __name__ == '__main__':
-  import uuid
-  app.secret_key = str(uuid.uuid4())
-  app.debug = False
-  app.run()
-
-'''import flask
-from flask import Flask
-from flask import request, redirect
-import json
-import httplib2
-import os
-
-from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
-
-import datetime
-app = Flask(__name__)
-
-@app.route("/")
-def hi():
-    return "yo this works"
-
-@app.route("/add_event", methods=['POST'])
-def add_event():
-    SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-    CLIENT_SECRET_FILE = 'client_secret.json'
-    APPLICATION_NAME = 'Google Calendar API Python Quickstart'
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'calendar-python-quickstart.json')
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('calendar', 'v3', http=http)
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    eventsResult = service.events().list(
-        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-        orderBy='startTime').execute()
-    events = eventsResult.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-    return "done"
-
-
-if __name__ == "__main__":
-	app.run()'''
+    import uuid
+    app.secret_key = str(uuid.uuid4())
+    app.debug = False
+    app.run()
