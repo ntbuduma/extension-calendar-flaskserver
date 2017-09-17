c254054499078d40e607d318c978a54968d6e30e
Nithin Buduma <nbuduma@mit.edu>
2017-09-16 21:00:39 -0400

    server should now authenticate hopefully

--- server_extension.py
+++ server_extension.py
@@ -1,6 +1,16 @@
+import flask
 from flask import Flask
-from flask import request
+from flask import request, redirect
 import json
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,8 +19,41 @@ def hi():
 
 @app.route("/add_event", methods=['POST'])
 def add_event():
-    request_data = request.get_data()
-    return json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True)
+    SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+    CLIENT_SECRET_FILE = 'client_secret.json'
+    APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+    return "done"
+
 
 if __name__ == "__main__":
 	app.run()
